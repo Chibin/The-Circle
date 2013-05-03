8bd3cebf9ae73b6fb0d69294c52060ef5f6f2a59
Marbin Tan <ayamaru@gmail.com>
2013-05-02 21:50:49 -0700

    Added Magic and battle menu.bmp

--- FinalProject/BattleScene.h
+++ FinalProject/BattleScene.h
@@ -8,7 +8,8 @@ class BattleScene : public Scene{
 	private:
 		TTF_Font* font;
 		SDL_Surface* textFight[2],* textRun[2],* textItem[2],* textDefend[2],* textMagic[2],* screen;
-		SDL_Rect fightLoc, runLoc, itemLoc, magicLoc;
+		SDL_Surface* bgBattleMenu;
+		SDL_Rect fightLoc, runLoc, itemLoc, magicLoc, bgBattleMenuLoc;
 		int fightVal, runVal,itemVal,magicVal;
 		enum battleSelect{FIGHT,ITEM,RUN,MAGIC, isFight,isItem,isRun, battleEnd,battlePhase,endPhase};
 		BattleManager* bManager;
