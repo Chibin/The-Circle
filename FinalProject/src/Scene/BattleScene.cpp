3d4321c1533a6534a9e9b7e017198d6003ca2583
Marbin Tan <ayamaru@gmail.com>
2013-05-03 16:23:06 -0700

    Fixed a bug where pressing return does not show up the endPhase text

--- FinalProject/src/Scene/BattleScene.cpp
+++ FinalProject/src/Scene/BattleScene.cpp
@@ -86,10 +86,12 @@ void BattleScene::eventHandler(SDL_Event& event){
 					case SDLK_RETURN:
 						bManager->battleHandler(battleMenu,screen);
 						if(battleMenu == battleEnd){
+							battleMenu = FIGHT;
 							GameManager::getInstance().setGameState(GameManager::OPENINGMENU); //temporary
-							battleMenu = FIGHT;	
 						}
 						else if(battleMenu == battlePhase)
+							bManager->battlePhaseUpdate(battleMenu,screen);
+						else if(battleMenu == endPhase)
 							bManager->battlePhaseUpdate(battleMenu,screen);
 						break;
 					case SDLK_z:
