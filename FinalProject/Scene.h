3c90a2a439abe28d77e4e4d04a3439ed88efd797
Adrian Wang <splinter500@gmail.com>
2013-05-03 00:09:33 -0700

    small cleanup

--- FinalProject/Scene.h
+++ FinalProject/Scene.h
@@ -4,11 +4,13 @@
 #include <SDL.h>
 #include <SDL_ttf.h>
 using namespace std;
+#include "GameManager.h"
 class Scene{
 	private:
+		GameManager instance;
 	public:
 		SDL_Surface* screen;
-		Scene(){}
+		Scene(){ instance = GameManager::getInstance();}
 		virtual void eventHandler(SDL_Event& event) = 0;
 		virtual void display(SDL_Surface* screen) = 0;
 };
